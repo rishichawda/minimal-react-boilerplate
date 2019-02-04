4b4c516e2c3adc833cda0df4a372c00a4347ab2e
rishichawda <chawda.rishii@gmail.com>
2019-02-04 22:13:01 +0530

    feat(root): add boilerplate code

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './src/App'
+
+import './index.scss'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './App.scss'
+
+const App = () => (
+  <div>
+    Welcome to React!
+  </div>
+)
+
+export default App
